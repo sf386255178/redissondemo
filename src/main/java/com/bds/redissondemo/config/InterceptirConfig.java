f822fd5d2fc29fc691b12a53833ecbbab576447a
Kevin.Ding <kevin.ding@bds-analytics.com>
2020-09-11 13:49:14 +0800

    [update]添加前端页面配置

--- src/main/java/com/bds/redissondemo/config/InterceptirConfig.java
+++ src/main/java/com/bds/redissondemo/config/InterceptirConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -17,9 +18,25 @@ public class InterceptirConfig implements WebMvcConfigurer {
 //    @Autowired
 //    private AuthenticationInterceptor interceptor;
 
+    /**
+     * token校验
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry  registry) {
-        registry.addInterceptor(authenticationInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(authenticationInterceptor())//添加自己的拦截器
+                .addPathPatterns("/**")//需要拦截的请求
+                .excludePathPatterns("/toHello");//放行的请求
+    }
+
+    /**
+     * 静态资源
+     * 因为WebMvcConfigurationSupport不走自动化的配置文件，所以一些静态文件，视图配置需要自己手动再添加一下
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/");
     }
 
     @Bean
